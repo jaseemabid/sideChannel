decfee4fd35ae6032c8fc5f165ff60fe242f6da3
Jaseem Abid <jaseemabid@gmail.com>
2012-10-29 04:15:59 +0530

    Plotting a simple amplitude X time graph

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 #! /usr/bin/env python2
 
+import sys
 import wave
-w = wave.open('./assets/left.wav', 'r')
+
+import array
+
+import matplotlib.pyplot as plt
 
 def getWaveInfo(w):
         """
@@ -16,11 +20,28 @@ def getWaveInfo(w):
         print "Compression type   : ", w.getcomptype()
         print "compression name   : ", w.getcompname()
 
+def plotWave(w):
+
+        nchannels, sampwidth, framerate, nframes, comptype, compname = w.getparams()
+
+        # Read the whole WAV at once
+        frames = w.readframes(nframes)
+        w.close()
+
+        # Convert to 16bit samples
+        samples = array.array("h", frames)
+
+        plt.plot(samples)
+        plt.ylabel('Amplitude')
+        plt.show()
 
 
 def main():
+
+        w = wave.open('./assets/left.wav', 'r')
+
         getWaveInfo(w)
+        plotWave(w)
 
-
-if __name__ == "__main__":
+if __name__ == "__main__" :
         main()
