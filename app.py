d7b39ca1888e3afb2dc4f1faec6823dff6f94719
Jaseem Abid <jaseemabid@gmail.com>
2012-10-29 01:48:51 +0530

    + getWaveInfo()

---
+++ app.py
@@ -0,0 +1,22 @@
+import wave
+w = wave.open('left.wav', 'r')
+
+def getWaveInfo(w):
+        """
+
+        Arguments:
+        - `w`: Wave_read instance
+        """
+        print ("Num of channels    : ", w.getnchannels())
+        print ("Sample Width       : ", w.getsampwidth())
+        print ("Sampling Frequency : ", w.getframerate())
+        print ("Number of frames   : ", w.getnframes())
+        print ("Compression type   : ", w.getcomptype())
+        print ("compression name   : ", w.getcompname())
+
+def main():
+        getWaveInfo(w)
+
+
+if __name__ == "__main__":
+        main()
